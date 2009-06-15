c75592952016342baeda3013ad1368392dea4b49
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-06-15 01:06:08 -0700

    add license to mac code

--- mac/AppController.m
+++ mac/AppController.m
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 OpenDNS, LLC. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import "AppController.h"
 
 @interface AppController (Private)
--- mac/main.m
+++ mac/main.m
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 OpenDNS, LLC. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
