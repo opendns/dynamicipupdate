f4a7624dc73413b8d8fca168b3dcec9bb2a01b2d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-07-17 17:21:03 -0700

    bump version to 2.0b8

--- windows/build-installer.bat
+++ windows/build-installer.bat
@@ -3,29 +3,16 @@
 @call vc9.bat
 @IF ERRORLEVEL 1 goto NO_VC9
 
-@pushd .
-@set VERSION=%1
-@IF NOT DEFINED VERSION GOTO VERSION_NEEDED
+@rem @pushd .
+@rem @set VERSION=%1
+@rem @IF NOT DEFINED VERSION GOTO VERSION_NEEDED
 
 @rem check if makensis exists
 @makensis /version >nul
 @IF ERRORLEVEL 1 goto NSIS_NEEDED
 
-python checkver.py %VERSION%
-@IF ERRORLEVEL 1 goto BAD_VERSION
-
-devenv OpenDNSUpdater.sln /Project UpdaterUI\UpdaterUI.vcproj /ProjectConfig Release /Rebuild
-@rem devenv OpenDNSUpdater.sln /Project UpdaterUI\UpdaterUI.vcproj /ProjectConfig Debug /Rebuild
+python buildandupload.py
 @IF ERRORLEVEL 1 goto BUILD_FAILED
-echo Compilation ok!
-
-@rem TODO: should I sign the binaries as well?
-
-@makensis /DVERSION=%VERSION% installer
-@IF ERRORLEVEL 1 goto INSTALLER_FAILED
-
-signtool sign /f opendns-sign.pfx /p bulba /d "OpenDNS Updater" /du "http://www.opendns.com/support/" /t http://timestamp.comodoca.com/authenticode OpenDNS-Updater-%VERSION%.exe
-@IF ERRORLEVEL 1 goto SIGN_FAILED
 
 @goto END
 
@@ -49,8 +36,8 @@ echo Need to provide version number e.g. build-release.bat 1.0
 echo NSIS doesn't seem to be installed. Get it from http://nsis.sourceforge.net/Download
 @goto END
 
-:SIGN_FAILED
-@echo Failed to sign the installer
+:BUILD_FAILED
+@rem @echo Failed to build
 @goto END
 
 :BAD_VERSION
