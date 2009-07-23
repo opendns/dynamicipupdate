a32eb58c5f635a2d03c9d422a50aec1b7ceccc70
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-07-23 13:15:55 -0700

    update version to 2.0b10

--- windows/src/MiscUtil.h
+++ windows/src/MiscUtil.h
@@ -20,7 +20,7 @@
 
 extern UINT g_errorNotifMsg;
 
-#define PROGRAM_VERSION  _T("2.0b9")
+#define PROGRAM_VERSION  _T("2.0b10")
 
 // PROGRAM_VERSION_SIMULATE_UPGRADE must be lower than PROGRAM_VERSION
 #define PROGRAM_VERSION_SIMULATE_UPGRADE _T("0.8")
