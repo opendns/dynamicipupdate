b4b9dd5f485424b3ca3a6ba54bb776e54fec8883
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-09-09 16:10:44 -0700

    update copyright notice to reflect LLC => Inc. transition

--- mac/NSDictionary+Networks.h
+++ mac/NSDictionary+Networks.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 OpenDNS Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <Cocoa/Cocoa.h>
 
 NSArray *labeledDynamicNetworks(NSDictionary *networksDict);
--- mac/NSDictionary+Networks.m
+++ mac/NSDictionary+Networks.m
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 OpenDNS Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import "NSDictionary+Networks.h"
 
 static BOOL isDynamicNetwork(NSDictionary *network) {
