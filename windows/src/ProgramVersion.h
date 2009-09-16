0cd4e6ed0da9b7d946dabb0e891a1ee9afec81eb
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-09-16 12:11:26 -0700

    forgot to add ProgramVersion.h

---
+++ windows/src/ProgramVersion.h
@@ -0,0 +1,14 @@
+#ifndef PROGRAM_VERSION_H__
+#define PROGRAM_VERSION_H__
+
+// Those are used in OpenDNSUpdater.rc to stamp the file version in exectuable
+#define VERSION_RC 2,0,1,0
+#define VERSION_RC_STR "2.0.1.0"
+
+#define PROGRAM_VERSION  _T("2.0.1")
+
+// PROGRAM_VERSION_SIMULATE_UPGRADE must be lower than PROGRAM_VERSION
+#define PROGRAM_VERSION_SIMULATE_UPGRADE _T("0.8")
+
+#endif
+
