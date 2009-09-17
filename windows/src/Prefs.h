2a0175c3890f9decf473057aa3f12fea4e309234
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-09-16 17:29:04 -0700

    add setting for supporesing ip mismatch error

--- windows/src/Prefs.h
+++ windows/src/Prefs.h
@@ -34,7 +34,8 @@ typedef struct Prefs {
 	M(network_id, NULL, false) \
 	M(user_networks_state, NULL, false) \
 	M(send_updates, "1", false) \
-	M(run_hidden, "0", false)
+	M(run_hidden, "0", false) \
+	M(suppress_error_ip_mismatch, "0", false)
 
 // g_pref_hostname - NULL means invalid, empty string means default
 
