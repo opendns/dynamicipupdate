5976dbcb3998facd369fdf137adb7cc083c0f4fa
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-10-06 11:00:34 -0700

    update version to 2.2b1

--- windows/src/ProgramVersion.h
+++ windows/src/ProgramVersion.h
@@ -2,10 +2,10 @@
 #define PROGRAM_VERSION_H__
 
 // Those are used in OpenDNSUpdater.rc to stamp the file version in exectuable
-#define VERSION_RC 2,1,0,0
-#define VERSION_RC_STR "2.1.0.0"
+#define VERSION_RC 2,2,0,0
+#define VERSION_RC_STR "2.2.0.0"
 
-#define PROGRAM_VERSION  _T("2.1")
+#define PROGRAM_VERSION  _T("2.2b1")
 
 // PROGRAM_VERSION_SIMULATE_UPGRADE must be lower than PROGRAM_VERSION
 #define PROGRAM_VERSION_SIMULATE_UPGRADE _T("0.8")
