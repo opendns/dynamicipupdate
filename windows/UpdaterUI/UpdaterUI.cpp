3b0177f53f9ed4b048c4650e644dc6b19b401f53
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-10-19 17:04:56 -0700

    (non-working) setting of DNS server

--- windows/UpdaterUI/UpdaterUI.cpp
+++ windows/UpdaterUI/UpdaterUI.cpp
@@ -449,16 +449,120 @@ static bool IsApiKeyValid(char *apiKey)
 }
 
 #if 1
-// We don't want to change the DNS settings of VirtualBox and VMWare virtual ethernet adapters
-BOOL ShouldSkipNetworkAdapter(WCHAR *caption)
+static void SetDnsServers(IWbemServices *pSvc, IWbemClassObject *pObj, OLECHAR *srv1, OLECHAR *srv2)
 {
+	HRESULT			hr;
+	SAFEARRAYBOUND	parrayBound;
+	VARIANT			vtProp;
+	_variant_t		serverList;
+	int				srvCount = 0;
+	CString			methodPath;
+	BSTR			methodName = SysAllocString(L"SetDNSServerSearchOrder");
+	BSTR			methodPathBstr = NULL;
+	IWbemClassObject *pAdapterClassObj = NULL;
+	IWbemClassObject *pAdapterClassInst = NULL;
+	IWbemClassObject *pSetDNSServerSearchOrderIn = NULL;
+	IWbemClassObject *pSetDNSServerSearchOrderOut = NULL;
+
+	VariantInit(&vtProp);
+	hr = pObj->Get(L"Index", 0, &vtProp, 0, 0);
+	if (FAILED(hr))
+		goto Exit;
+
+	assert(vtProp.vt == VT_I4);
+	if (vtProp.vt != VT_I4)
+		goto Exit;
+
+	int idx = vtProp.lVal;
+
+	hr = pSvc->GetObject(
+		bstr_t("Win32_NetworkAdapterConfiguration"),
+		WBEM_FLAG_RETURN_WBEM_COMPLETE,
+		NULL,
+		&pAdapterClassObj,
+		NULL);
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = pAdapterClassObj->GetMethod(methodName, 0, &pSetDNSServerSearchOrderIn, NULL);
+	if (FAILED(hr))
+		goto Exit;
+
+	hr = pAdapterClassObj->SpawnInstance(0, &pAdapterClassInst);
+	if (FAILED(hr))
+		goto Exit;
+
+	if (srv1)
+		++srvCount;
+	if (srv2)
+		++srvCount;
+	if (0 == srvCount)
+		return;
+
+	parrayBound.cElements = srvCount;
+	parrayBound.lLbound = 0;
+	serverList.vt = VT_ARRAY | VT_BSTR;
+	if (0 == srvCount) {
+		serverList.parray = NULL;
+	} else {
+		serverList.parray = ::SafeArrayCreateEx(VT_BSTR, 1, &parrayBound, NULL);
+
+		BSTR* dnsServerAddress;
+		::SafeArrayAccessData(serverList.parray, reinterpret_cast<LPVOID*>(&dnsServerAddress));
+		int index = 0;
+		if (srv1)
+		{
+			dnsServerAddress[index] = ::SysAllocString(srv1);
+			index++;
+		}
+		if (srv2) 
+		{
+			dnsServerAddress[index] = ::SysAllocString(srv2);
+			index++;
+		}
+		::SafeArrayUnaccessData(serverList.parray);
+	}
+
+	hr = pAdapterClassObj->Put(bstr_t("DNSServerSearchOrder"), 0, &serverList, 0);
+	if (FAILED(hr))
+		goto Exit;
+
+	methodPath.Format("Win32_NetworkAdapterConfiguration.Index='%d'", (int)idx);
+	methodPath.SetSysString(&methodPathBstr);
+	hr = pSvc->ExecMethod(methodPathBstr, methodName, 0, NULL, pSetDNSServerSearchOrderIn, &pSetDNSServerSearchOrderOut, NULL);
+	if (FAILED(hr))
+		goto Exit;
+Exit:
+	if (pAdapterClassObj) pAdapterClassObj->Release();
+	if (pAdapterClassInst) pAdapterClassInst->Release();
+	if (pSetDNSServerSearchOrderOut) pSetDNSServerSearchOrderOut->Release();
+	if (pSetDNSServerSearchOrderIn) pSetDNSServerSearchOrderIn->Release();
+	::SysFreeString(methodName);
+	::SysFreeString(methodPathBstr);
+}
+
+static void ClearDnsServers(IWbemServices *pSvc, IWbemClassObject *pObj)
+{
+	SetDnsServers(pSvc, pObj, NULL, NULL);
+}
+
+// We don't want to change the DNS settings of VirtualBox/VMWare etc. virtual ethernet adapters
+// I wish I knew a better heurstic to filter non-physical adapters
+static BOOL ShouldSkipNetworkAdapter(WCHAR *caption)
+{
+	// virtual adapter from VirtualBox
 	if (-1 != WStrFind(caption, L"VirtualBox"))
 		return TRUE;
-	// TODO: add VMWare names
+	// virtual adapter from coLinux (http://colinux.wikia.com/wiki/TAP-Win32_Adapter_V8_%28coLinux%29)
+	if (-1 != WStrFind(caption, L"TAP-Win32"))
+		return TRUE;
+	// virtual adapter from VMWare
+	if (-1 != WStrFind(caption, L"VMware"))
+		return TRUE;
 	return FALSE;
 }
 
-int WINAPI _tWinMain(HINSTANCE  /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTSTR /* cmdLine */, int /* nCmdShow */)
+void SetOpenDnsServersOnAllAdapters()
 {
 	IWbemLocator *pLoc = NULL;
 	IWbemServices *pSvc = NULL;
@@ -466,7 +570,7 @@ int WINAPI _tWinMain(HINSTANCE  /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTS
 
 	HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED); 
 	if (FAILED(hr))
-		return 1;
+		return;
 
 	hr =  CoInitializeSecurity(
 		NULL, 
@@ -496,7 +600,7 @@ int WINAPI _tWinMain(HINSTANCE  /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTS
 	// the current user and obtain pointer pSvc
 	// to make IWbemServices calls.
 	hr = pLoc->ConnectServer(
-		 _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+		 bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
 		 NULL,					  // User name. NULL = current user
 		 NULL,					  // User password. NULL = current
 		 0, 					  // Locale. NULL indicates current
@@ -537,32 +641,50 @@ int WINAPI _tWinMain(HINSTANCE  /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTS
 	if (!pEnumerator)
 		goto Exit;
 
-	IWbemClassObject *pclsObj;
-	ULONG uReturn = 0;
+	IWbemClassObject *pObj;
+	ULONG result = 0;
 
 	for (;;)
 	{
 		VARIANT vtProp;
-		hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
-		if (0 == uReturn || FAILED(hr))
+		WCHAR *s;
+		VariantInit(&vtProp);
+		hr = pEnumerator->Next(WBEM_INFINITE, 1, &pObj, &result);
+		if (0 == result)
 			break;
 
-		hr = pclsObj->Get(L"IPEnabled", 0, &vtProp, 0, 0);
+		hr = pObj->Get(L"IPEnabled", 0, &vtProp, 0, 0);
 		if (FAILED(hr) || !vtProp.boolVal)
 			goto Next;
 
+#if 0
 		VariantClear(&vtProp);
-		hr = pclsObj->Get(L"Caption", 0, &vtProp, 0, 0);
+		hr = pObj->Get(L"SettingID", 0, &vtProp, 0, 0);
 		if (FAILED(hr))
 			goto Next;
+		s = vtProp.bstrVal;
+#endif
 
-		WCHAR *s = vtProp.bstrVal;
+		VariantClear(&vtProp);
+		hr = pObj->Get(L"Caption", 0, &vtProp, 0, 0);
+		if (FAILED(hr))
+			goto Next;
+		s = vtProp.bstrVal;
+
 		if (ShouldSkipNetworkAdapter(s))
 			goto Next;
-		// TODO: set the DNS settings
+
+#if 0
+		VariantClear(&vtProp);
+		hr = pObj->Get(L"DNSServerSearchOrder", 0, &vtProp, 0, 0);
+		if (FAILED(hr))
+			goto Next;
+#endif
+
+		SetDnsServers(pSvc, pObj, OLESTR("208.67.222.222"), OLESTR("208.67.220.220 "));
 Next:
 		VariantClear(&vtProp);
-		pclsObj->Release();
+		pObj->Release();
 	}
 
 Exit:
@@ -571,7 +693,12 @@ Exit:
 	if (pLoc) pLoc->Release();
 
 	CoUninitialize();
-	return 0;
+	return;
+}
+
+int WINAPI _tWinMain(HINSTANCE  /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPTSTR /* cmdLine */, int /* nCmdShow */)
+{
+	SetOpenDnsServersOnAllAdapters();
 }
 #else
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR cmdLine, int /* nCmdShow */)
